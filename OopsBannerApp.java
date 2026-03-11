a8c797bd58acf68a3c6e7d63f322133410f70017
Naman79397 <wpass5096@gmail.com>
2026-03-11 18:54:21 +0530

    UC2 - Render OOPS as Banner using Print Statements

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,6 +1,13 @@
 public class OopsBannerApp {
-    public static void main(String[]args){
-        System.out.println( "OOPS");
-
+    public static void main(String[] args) {
+        // Construct the banner line by line using string concatenation [cite: 58, 60]
+        
+        System.out.println("  ** " + "  ** " + "  ** " + "  ** "); 
+        System.out.println(" * * " + " * * " + " * * " + " * "); 
+        System.out.println(" * * " + " * * " + " * * " + " * "); 
+        System.out.println(" * * " + " * * " + " * * " + "  *** "); 
+        System.out.println(" * * " + " * * " + " ** " + "       * "); 
+        System.out.println(" * * " + " * * " + " * " + "       * "); 
+        System.out.println("  ** " + "  ** " + " * " + "  *** "); 
     }
 }
